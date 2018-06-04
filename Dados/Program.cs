48838934c31f9289b57a2907a3909cbcba64dfdb
Ramon Chiara <ramonchiara@gmail.com>
2018-06-04 01:01:28 -0300

    Programa mostrando que o problema voltou.

--- Dados/Program.cs
+++ Dados/Program.cs
@@ -6,10 +6,11 @@ namespace Dados
 	{
 		public static void Main(string[] args)
 		{
-			Dado dado = new Dado(6);
+			Dado dado1 = new Dado(6);
+			Dado dado2 = new Dado(6);
 
 			for (int i = 0; i < 10; i++) {
-				Console.WriteLine(dado.Lancar());
+				Console.WriteLine("{0} {1}", dado1.Lancar(), dado2.Lancar());
 			}
 		}
 	}
