d744856990b14a9699901d626667671b2fe117b2
Ramon Chiara <ramonchiara@gmail.com>
2018-06-04 00:57:19 -0300

    Programa mostrando o problema.

---
+++ Dados/Dado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dados
+{
+	public class Dado
+	{
+		private int lados;
+
+		public Dado(int lados)
+		{
+			this.lados = lados;
+		}
+
+		public int Lancar()
+		{
+			Random rng = new Random();
+			return rng.Next(1, lados + 1);
+		}
+	}
+}
---
+++ Dados/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dados
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++) {
+				Dado dado = new Dado(6);
+				Console.WriteLine(dado.Lancar());
+			}
+		}
+	}
+}
